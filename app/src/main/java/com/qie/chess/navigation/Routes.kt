3c0a48c9ced1baaf3401e29faf3fffabe402d32e
Arzaqiee <luaygaming286@gmail.com>
2026-09-12 20:18:54 +0700

    chore: upload 76 file via GitUp

---
+++ app/src/main/java/com/qie/chess/navigation/Routes.kt
@@ -0,0 +1,13 @@
+package com.qie.chess.navigation
+
+object Routes {
+    const val MAIN_MENU = "main_menu"
+    const val BOT_SETUP = "bot_setup"
+    const val LOCAL_SETUP = "local_setup"
+    const val ONLINE_LOBBY = "online_lobby"
+    const val GAME = "game"
+    const val PUZZLE_LIST = "puzzle_list"
+    const val PUZZLE_GAME = "puzzle_game"
+    const val HISTORY = "history"
+    const val SETTINGS = "settings"
+}
